f9b51755b4e13cfd5fabefa8faad3cdef215f902
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-05 15:31:18 +0200

    Added img folder from prev slider and new array

--- script.js
+++ script.js
@@ -3,7 +3,30 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        activeIndex: 0,
+        imagesSlider: [
+            {
+                image: 'img/01.webp',
+                title: 'Marvel\'s Spiderman Miles Morale',
+                text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+            }, {
+                image: 'img/02.webp',
+                title: 'Ratchet & Clank: Rift Apart',
+                text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+            }, {
+                image: 'img/03.webp',
+                title: 'Fortnite',
+                text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+            }, {
+                image: 'img/04.webp',
+                title: 'Stray',
+                text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+            }, {
+                image: 'img/05.webp',
+                title: "Marvel's Avengers",
+                text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+            }
+        ],
       }
     }
   }).mount('#app')
